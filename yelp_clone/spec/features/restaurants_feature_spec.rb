55c1d711c6f3f551b11aeb96f8dfcdd7ac8d6d90
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-09 11:54:06 +0000

    created view using haml

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'restaurants' do
-  context 'no restaurants have been added' do
+  context 'No restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
