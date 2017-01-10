06d471453661add63abd85cba85b4f7be0a24eac
TudorTacal <tudortacal@gmail.com>
2017-01-10 12:48:41 +0000

    Reading restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -33,4 +33,17 @@ feature 'restaurants' do
       end
     end
 
+    context 'viewwing restaurants' do
+
+      let!(:kfc){Restaurant.create(name: 'KFC')}
+
+      scenario 'lets a user view a restaurant' do
+        visit '/restaurants'
+        click_link 'KFC'
+        expect(page).to have_content 'KFC'
+        expect(current_path).to eq "/restaurants/#{kfc.id}"
+      end
+
+    end
+
 end
