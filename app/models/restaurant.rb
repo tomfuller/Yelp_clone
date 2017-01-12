028df7d7edf820fbfb3bd594ba3a5cae27d6bfa9
Tom Fuller <tomf95@hotmail.co.uk>
2017-01-12 12:39:21 +0000

    extended active record associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,11 @@
 class Restaurant < ApplicationRecord
 
   belongs_to :user
-  has_many :reviews
 
-  has_many :reviews, dependent: :destroy
+  has_many :reviews,
+  -> { extending WithUserAssociationExtension },
+   dependent: :destroy
+
   validates :name, length: {minimum: 3}, uniqueness: true
 
   def build_review(attributes = {}, user)
